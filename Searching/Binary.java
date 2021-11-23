a76a25058f04c2d6afe5bbb6931ee9f9cb05d4c9
Madhavi Sai <76171456+Madhavi-sai@users.noreply.github.com>
2021-11-23 11:28:17 +0000

    finished searching

--- Searching/Binary.java
+++ Searching/Binary.java
@@ -1,10 +1,31 @@
-class Java{
-  public static void main(String args[]){
-    
-    int arr[] = {0, 1, 2, 3, 4}
-    int high = 4;
-    int low = 0;
-    int mid = (high + low)
 
-  }
+class Binary{
+    int binarySearch(int arr[], int x)
+    {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == x)
+                return mid;
+            if (arr[mid] < x)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+ 
+    public static void main(String args[])
+    {
+        Binary object = new Binary();
+        int arr[] = { 0, 1, 2, 3, 10};
+        int n = arr.length;
+        int key = 10;
+        int result = object.binarySearch(arr, key);
+        if (result == -1)
+            System.out.println("Not found");
+        else
+            System.out.println("Element found at index " + result);
+    }
 }
+
--- Searching/LinearSearch.java
+++ Searching/LinearSearch.java
@@ -1,1 +1,22 @@
+    class LinearSearch{    
+      int linear(int[] arr, int key){    
+          for(int i=0;i<arr.length;i++){    
+              if(arr[i] == key){    
+                  return i;    
+                }    
+            }    
+            return -1;    
+        }   
 
+
+        public static void main(String args[]){ 
+            LinearSearch object = new LinearSearch();   
+            int[] array= {10,20,30,50,70,90};    
+            int key = 50;
+            int result = object.linear(array, key);
+            if (result == -1)
+              System.out.println("Not found");
+            else
+              System.out.println("Element found at index " + result); 
+        }    
+    }    
